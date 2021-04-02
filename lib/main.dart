0940dddb6c288787846863a1c77c60a48e5f2888
Amar Nath <nthvpsn@gmail.com>
2021-04-02 09:02:42 +0530

    Add Icon

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz App"),
+        ),
+      ),
+    );
+  }
+}
